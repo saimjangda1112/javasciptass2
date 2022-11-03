8f4ac6b8de2ad8afae3f8cafb2d0300f726dc6be
saimjangda1112 <115483392+saimjangda1112@users.noreply.github.com>
2022-11-03 16:15:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+var subject1 = +prompt(" Enter english marks")
+    var subject2 = +prompt(" Enter science marks")
+    var subject3 = +prompt(" Enter maths marks")
+    var subject4 = +prompt("Enter urdu marks")
+    var subject5 = +prompt("Enter Islamiat marks")
+    var totalMarks = subject1+subject2+subject3+subject4+subject5
+    var Per=totalMarks /250*190
+     console.log ('percentage== >',Per)
+    if (Per>50 && Per<60) {
+    console.log("pass Grade D")
+    }else if (Per>70 && Per<80){
+    console.log("pass Grade C");
+    } else if (Per >80 && Per<90){
+    console. log("pass Grade A")
+    }else if (Per>90 && Per<100){ 
+    console. log(" pass Grade A*")
+    }
+
+    // console.log (subject1)
+    // console.log (subject2)
+    // console.log (subject3)
+    // console.log (subject4)
+    // console.log (subject5)
+    // var Money = false
+
+// console. log (subject1)
+// console. log (subject2)
+// console. log (subject3)
+// console. log (subject4)
+// console. log (subject5)
+// var Money = false
+// if (Money == true) {
+// console.log("Amount Received");
+// } else { 
+// console.log("return to your home")
+// }
+// var weight = prompt ( "Enter your weight" )
+// var GymCard = false
+
+// if (weight>90){
+// console.log("Person allowed")
+// }else if (GymCard == true) {
+// console.log("Person allowed")
+// } else {
+// console. log("Person not allowed")
+// }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
